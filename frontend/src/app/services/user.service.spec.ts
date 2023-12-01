222cd657d51f186404e55e38b7359d317bc64f7f
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-12-01 11:38:04 -0800

    git user service test

--- frontend/src/app/services/user.service.spec.ts
+++ frontend/src/app/services/user.service.spec.ts
@@ -1,61 +1,56 @@
-// import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
-// import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-// import { UserService } from './user.service';
-// import { UserDetails } from '../interfaces/user';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { UserService } from './user.service';
 
-// describe('UserService', () => {
-//   let service: UserService;
-//   let httpTestingController: HttpTestingController;
+describe('UserService', () => {
+  let service: UserService;
+  let httpTestingController: HttpTestingController;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule],
-//       providers: [UserService],
-//     });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
+    });
+    service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
 
-//     service = TestBed.inject(UserService);
-//     httpTestingController = TestBed.inject(HttpTestingController);
-//   });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
 
-//   afterEach(() => {
-//     httpTestingController.verify();
-//   });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
+  it('should get user details', (done) => {
+    const mockToken = 'mock-token';
+    localStorage.setItem('token', mockToken);
 
-//   it('should fetch user details and return the role', fakeAsync(() => {
-//     const mockResponse = { info: { role: 'admin' } };
-//     let role: string | undefined;
+    const mockResponse = { info: { role: 'admin' } };
 
-//     service.checkDetails().then((result) => {
-//       role = result;
-//     });
+    service.checkDetails().then((role) => {
+      expect(role).toEqual('admin');
+      done();
+    });
 
-//     const req = httpTestingController.expectOne('http://localhost:8000/user/check_user_details');
-//     expect(req.request.method).toEqual('GET');
+    const req = httpTestingController.expectOne('http://localhost:8000/user/check_user_details');
+    expect(req.request.method).toBe('GET');
+    expect(req.request.headers.get('token')).toBe(mockToken);
 
-//     req.flush(mockResponse);
+    req.flush(mockResponse);
+  });
 
-//     tick();
+  it('should get users', (done) => {
+    const mockUsers = [{ id: 1, name: 'User 1' }, { id: 2, name: 'User 2' }];
 
-//     expect(role).toEqual('admin');
-//   }));
+    service.getUsers().subscribe((users) => {
+      expect(users).toEqual(mockUsers);
+      done();
+    });
 
-//   it('should get users', () => {
-//     const mockUsers: UserDetails[] = [
-//       { id: 1, name: 'User 1', email: 'user1@example.com' },
-//       { id: 2, name: 'User 2', email: 'user2@example.com' },
-//     ];
-
-//     service.getUsers().subscribe((users) => {
-//       expect(users).toEqual(mockUsers);
-//     });
-
-//     const req = httpTestingController.expectOne('http://localhost:8000/user');
-//     expect(req.request.method).toEqual('GET');
-
-//     req.flush(mockUsers);
-//   });
-// });
+    const req = httpTestingController.expectOne('http://localhost:8000/user');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUsers);
+  });
+});
